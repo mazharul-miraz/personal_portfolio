ef46942f33059e65f2d779606b98f2b8361622b5
miraz <mazharul.miraz@outlook.com>
2021-08-15 00:22:31 +0900

    connect postgresql database

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'admin',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
